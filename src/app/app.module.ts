bd26a5eaf88d79f4c1f8bda9bdcde94bc38918c3
arpho <damicogiuseppe77@gmail.com>
2022-02-17 18:04:38 +0100

    Dynamic form module
    imported module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ItemModule } from './modules/item/item.module';
+import { DynamicFormModule } from './modules/dynamic-form/dynamic-form.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,ItemModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,ItemModule,DynamicFormModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
