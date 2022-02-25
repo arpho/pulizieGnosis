b67391af7d27eea4d28aa2fa5d6586d5774aabc1
arpho <damicogiuseppe77@gmail.com>
2022-02-25 19:29:57 +0100

    refactoring
    used new version of firebase in auth.service

--- src/app/modules/user/services/auth.service.ts
+++ src/app/modules/user/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import firebase from 'firebase/compat/app';
 import 'firebase/auth';
 import 'firebase/database';
+import {getAuth,signInWithEmailAndPassword,createUserWithEmailAndPassword,sendEmailVerification} from 'firebase/auth'
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,9 @@ export class AuthService {
   constructor() { }
 
   loginUser(email: string, password: string): Promise<any> {
-    return firebase.auth().signInWithEmailAndPassword(email, password);
+    const auth=getAuth()
+
+    return signInWithEmailAndPassword(auth,email,password);
   }
 
   resetPassword(email: string): Promise<void> {
@@ -19,16 +22,21 @@ export class AuthService {
   }
 
   signupUser(email: string, password: string): Promise<any> {
+    const auth = getAuth()
+    createUserWithEmailAndPassword(auth,email,password).then((newUserCredential=>{
+      sendEmailVerification(newUserCredential.user)
+    }))
     return firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
       .then(newUserCredential => {
-        newUserCredential.user.sendEmailVerification();
-        firebase
-        
+        newUserCredential.user.sendEmailVerification().then(()=>{
+          firebase
           .database()
           .ref(`/userProfile/${newUserCredential.user.uid}/email`)
           .set(email);
+        });
+        
       })
       .catch(error => {
         console.error(error);
